33aa258ab4bfa49eb207097d5115b0ebe84ab407
Chris <2071856+christiangroth@users.noreply.github.com>
2025-12-05 08:27:52 +0100

    Day 5, Part 1

---
+++ src/main/kotlin/de/chrgroth/adventofcode/puzzles/Day05.kt
@@ -0,0 +1,36 @@
+package de.chrgroth.adventofcode.puzzles
+
+import de.chrgroth.adventofcode.puzzles.utils.skipBlank
+import de.chrgroth.adventofcode.puzzles.utils.splitAtBlankLine
+
+
+data object Day05 : Puzzle {
+
+  override suspend fun solve(stage: Stage, input: List<String>): PuzzleSolution {
+
+    // The database operates on ingredient IDs.
+    // It consists of a list of fresh ingredient ID ranges, a blank line, and a list of available ingredient IDs
+    // The fresh ID ranges are inclusive: the range 3-5 means that ingredient IDs 3, 4, and 5 are all fresh.
+    // The ranges can also overlap
+    val (freshIdRanges, availableIds) = input.splitAtBlankLine().let { (freshIdRangesInput, availableIdsInput) ->
+      freshIdRangesInput.skipBlank().map { line ->
+        line.split('-')
+          .let {
+            check(it.size == 2) { "Invalid range: $line" }
+            it[0].toInt()..it[1].toInt()
+          }
+      } to availableIdsInput.skipBlank().map { line -> line.toInt() }
+    }
+
+    // an ingredient ID is fresh if it is in any range.
+    val freshIngredientIds = availableIds.filter { id -> freshIdRanges.any { range -> id in range } }
+
+    return PuzzleSolution(
+      freshIngredientIds.size, null
+    )
+  }
+}
+
+suspend fun main() {
+  Day05.run()
+}
--- src/main/kotlin/de/chrgroth/adventofcode/puzzles/utils/Input.kt
+++ src/main/kotlin/de/chrgroth/adventofcode/puzzles/utils/Input.kt
@@ -2,3 +2,15 @@ package de.chrgroth.adventofcode.puzzles.utils
 
 internal fun List<String>.skipBlank(): List<String> =
   filter { it.isNotBlank() }
+
+fun List<String>.splitAtBlankLine(): Pair<List<String>, List<String>> =
+  splitAt { it.isBlank() }
+
+fun List<String>.splitAt(predicate: (String) -> Boolean): Pair<List<String>, List<String>> {
+  val index = indexOfFirst(predicate)
+  return if (index == -1) {
+    this to emptyList()
+  } else {
+    subList(0, index) to subList(index + 1, size)
+  }
+}
