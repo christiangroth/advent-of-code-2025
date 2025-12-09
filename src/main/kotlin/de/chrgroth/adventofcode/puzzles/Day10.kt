86e876243b497b15346127eb62e891de4cdb7707
Chris <2071856+christiangroth@users.noreply.github.com>
2025-12-09 12:09:45 +0100

    Prepare next days

---
+++ src/main/kotlin/de/chrgroth/adventofcode/puzzles/Day10.kt
@@ -0,0 +1,20 @@
+package de.chrgroth.adventofcode.puzzles
+
+import de.chrgroth.adventofcode.puzzles.utils.skipBlank
+
+data object Day10 : Puzzle {
+
+  override suspend fun solve(stage: Stage, input: List<String>): PuzzleSolution {
+
+    val input = input.skipBlank()
+
+    return PuzzleSolution(
+      0,
+      null
+    )
+  }
+}
+
+suspend fun main() {
+  Day10.run()
+}
